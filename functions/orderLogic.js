ae1b75447aebf2d1a5e96e9905714dd77db47ca2
pkurto16 <ptkurto3@gmail.com>
2023-09-16 17:27:42 -0400

    added order logic for delivery as well

--- functions/orderLogic.js
+++ functions/orderLogic.js
@@ -9,6 +9,21 @@ const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
 
+
+function haversineDistance(geo1, geo2) {
+    const R = 3958.8;
+    const dLat = toRad(geo2.latitude - geo1.latitude);
+    const dLon = toRad(geo2.longitude - geo1.longitude);
+    const lat1 = toRad(geo1.latitude);
+    const lat2 = toRad(geo2.latitude);
+  
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+              Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    
+    return R * c;
+}
+
 exports.foo = onDocumentCreated("/orders/{orderId}", async (event) => {
     const orderDocument = event.data.data();
     console.log("this is the orderId: " + orderId);
@@ -20,8 +35,24 @@ exports.foo = onDocumentCreated("/orders/{orderId}", async (event) => {
             .where("latestTime", ">=", orderDocument.timeListed)
             .orderBy("basePrice");
     }
-    const listingsDocument = await getFirestore()
-        .collection("listings");
-        .where("offersDelivery", "==", "isDelivery")
+    else{
+        const listingDocument = await getFirestore()
+            .collection("listings")
+            .where("offersDelivery", "==", "isDelivery")
+            .where("diningCourt", "==", orderDocument.diningCourt)
+            .where("latestTime", ">=", orderDocument.timeListed)
+        let validListings = [];
+        for(let listing of listingDocument){
+            let distanceToLocation = haversineDistance(locationTable[listing.diningCourt], orderDocument.location);
+            if(distanceToLocation>range){
+                continue;
+            }
+            let realPrice = listing.basePrice+distanceToLocation*listing.milePrice;
+            listing.add(realPrice);
+            validListings.append(listing);
+        }
+        validListings.orderBy("realPrice");
+    }
+        
 
-})
+});
